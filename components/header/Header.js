5aea8025ebd5b9cea76e24aaeb0d30b3435e29c4
Nicolas Vallee <hello@nicolasvallee.dev>
2021-08-19 10:59:49 +0400

    add nav menu overlay

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,18 +1,59 @@
 import Image from 'next/image';
 import { useState } from 'react';
+import Link from 'next/link';
 import styles from './Header.module.scss';
 import { Squash as Hamburger } from 'hamburger-react';
+import classNames from 'classnames';
 
 const Header = (props) => {
   const [isOpen, setOpen] = useState(false);
+  var menuOverlayClass = classNames(styles.menuOverlay, {
+    [styles.menuOpen]: isOpen,
+  });
 
   return (
-    <header>
+    <header className={styles.header}>
       <div className={styles.bgStripe}></div>
-      <div className={styles.hamburgerIcon}>
-        <Hamburger toggled={isOpen} toggle={setOpen} rounded color='#f9fcff' />
+      <div className={menuOverlayClass}>
+        <div className={styles.menuItems}>
+          <nav>
+            <li>
+              <Link href='/'>
+                <a>What I Can Do For You</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/about'>
+                <a>About Me</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/projects'>
+                <a>My Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/contact'>
+                <a>Contact Me</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/blog'>
+                <a>Blog Posts & Case Studies</a>
+              </Link>
+            </li>
+          </nav>
+        </div>
       </div>
       <div className={styles.container}>
+        <div className={styles.hamburgerIcon}>
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            rounded
+            color='#f9fcff'
+          />
+        </div>
         <div className={styles.avatarContainer}>
           <Image
             src='/images/Nicolas Vallee Web Developer.png'
