0bb596ea4640e09ceddaee115c9beec907e26aec
Nicolas Vallee <hello@nicolasvallee.dev>
2021-08-25 08:37:06 +0400

    hide contact form to be implemented later

--- components/views/ContactPage.js
+++ components/views/ContactPage.js
@@ -9,10 +9,24 @@ const ContactPage = () => {
         <div style={{ width: '100%' }}>
           <TextContent col4lg centered>
             <h2>Send me a message</h2>
+            <p style={{ color: '#5f6a74', fontStyle: 'italic' }}>
+              In an effort to deploy this site ASAP, I left a few features to be
+              implemented post-launch, like this contact form.
+            </p>
+            <p style={{ color: '#5f6a74', fontStyle: 'italic' }}>
+              In the meantime, you can send me an email at:{' '}
+              <a href='mailto:hello@nicolasvallee.dev'>
+                hello@nicolasvallee.dev
+              </a>
+              .
+            </p>
+            <p style={{ color: '#5f6a74', fontStyle: 'italic' }}>
+              Sorry for the inconvenience.
+            </p>
           </TextContent>
-          <div className={`col4lg centered`}>
+          {/* <div className={`col4lg centered`}>
             <ContactForm />
-          </div>
+          </div> */}
         </div>
       </PageSection>
       <PageSection>
